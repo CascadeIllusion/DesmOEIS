7e2f3e7b31bf2cba6cd52a8b137c82f77516d9b7
CascadeIllusion <patrick77000+1@gmail.com>
2020-07-05 16:57:48 -0500

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DesmOEIS',
+      version='1.0',
+      description='Tool for converting OEIS sequences to Desmos lists',
+      author='CascadeIllusion',
+      author_email='patrick77000@gmail.com',
+      url='https://github.com/CascadeIllusion/DesmOEIS',
+      packages=['src', 'tests'],
+     )
